b5aaf31a1fe720df5971ccc10463693cde2c2d0a
Theodoros <theodorosai26@TheodorosAI26.attlocal.net>
2026-03-13 16:35:35 -0500

    Add debug logging

--- api-signalwire.cjs
+++ api-signalwire.cjs
@@ -1,16 +1,21 @@
+console.log('STARTING SERVER...');
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+console.log('PORT:', PORT);
+
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/health', (req, res) => {
+  console.log('HEALTH CHECK');
   res.json({ status: 'ok' });
 });
 
 app.post('/webhook/sms', (req, res) => {
+  console.log('SMS RECEIVED:', req.body);
   const { From, Body } = req.body;
-  console.log(`SMS from ${From}: ${Body}`);
   
   res.type('text/xml');
   res.send(`<?xml version="1.0" encoding="UTF-8"?><Response><Message>Received: ${Body}</Message></Response>`);
